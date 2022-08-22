dcf2dbe4438643d0f1df270c7c067982d3a81712
VashuRaj <111109980+VashuRaj@users.noreply.github.com>
2022-08-22 11:10:25 +0530

    Update stream.py

--- WebStreamer/bot/plugins/stream.py
+++ WebStreamer/bot/plugins/stream.py
@@ -25,7 +25,7 @@ async def private_receive_handler(c: Client, m: Message):
             if user.status == "kicked":
                 await c.send_message(
                     chat_id=m.chat.id,
-                    text="Sorry Sir, You are Banned to use me. Contact my [Support Group](https://t.me/TeleRoid14).",
+                    text="Sorry Sir, You are Banned to use me. Contact my [Support Group](https://t.me/DeltaBotsOfficial).",
                     parse_mode="markdown",
                     disable_web_page_preview=True
                 )
@@ -33,7 +33,7 @@ async def private_receive_handler(c: Client, m: Message):
         except UserNotParticipant:
             await c.send_message(
                 chat_id=m.chat.id,
-                text="**Please Join My Updates Channel to use this Bot Dude!**\n\nDue to Overload, Only Channel Subscribers can use the Bot!\n\n@TheTeleRoid",
+                text="**Please Join My Updates Channel to use this Bot Dude!**\n\nDue to Overload, Only Channel Subscribers can use the Bot!\n\n@DeltaBotsOfficial",
                 reply_markup=InlineKeyboardMarkup(
                     [
                         [
@@ -47,7 +47,7 @@ async def private_receive_handler(c: Client, m: Message):
         except Exception:
             await c.send_message(
                 chat_id=m.chat.id,
-                text="Something went Wrong. Contact my [Support Group](https://t.me/TeleRoid14).",
+                text="Something went Wrong. Contact my [Support Group](https://t.me/DeltaBotsOfficial).",
                 parse_mode="markdown",
                 disable_web_page_preview=True)
             return
@@ -103,7 +103,7 @@ async def channel_receive_handler(bot, broadcast):
             message_id=broadcast.message_id,
             reply_markup=InlineKeyboardMarkup(
                 [
-                    [InlineKeyboardButton("Get Direct Download Link", url=f"https://t.me/{(await bot.get_me()).username}?start=PredatorHackerzZ_{str(log_msg.message_id)}")]
+                    [InlineKeyboardButton("Get Direct Download Link", url=f"https://t.me/{(await bot.get_me()).username}?start=DeltaXfileStreamBot_{str(log_msg.message_id)}")]
                 ]
             )
         )
